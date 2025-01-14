ec5819f69398efeb227addfda01c3c66cf48dbdb
Adria Freitas <adriafreitas2908@gmail.com>
2025-01-14 12:16:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,14 +24,15 @@ operadores = {"+": "Soma", "-": "Subtração", "*": "Multiplicação", "/": "Div
 
 # Loop principal do programa.
 while True:
-
+    
     # Exibe as operações disponíveis.
-    print("Operações Disponíveis:")
+    print("Operações Disponíveis: \n")
     for op, desc in operadores.items():
         print(f"{op} : {desc}")
-    
+    print("\n")
     # Solicita ao usuário que escolha uma operação.
     operador = input("Digite a operação que deseja realizar: ")
+
     if operador not in operadores:
         print("\nDigite uma operação válida!") # Mensagem de erro para operador inválido.
         continue # Reinicia o loop.
@@ -48,15 +49,15 @@ while True:
     print("\n")
 
     # Inicializa a variável de resposta do usuário.          
-    resp = input("Deseja realizar uma nova operação (N ou S)? ").strip().upper()
+    resp = input("Deseja realizar uma nova operação? 1-SIM, 0-NÃO: ").strip()
     # Verifica se a resposta do usuário é válida.
-    while resp not in ["S", "N", "s", "n"]:
-            print("Digite S ou N")
-            resp = input("Deseja realizar uma nova operação (N ou S)? ").strip().upper()
-    # Se o usuário digitar "N", encerra o programa.
-    if resp == "N":
+    while resp not in ['0', '1']:
+            print("Digite 0 ou 1")
+            resp = input("Deseja realizar uma nova operação? 1-SIM, 0-NÃO: ").strip()
+    # Se o usuário digitar "0", encerra o programa.
+    if resp == "0":
         os.system("cls") # Limpa a tela.
         break
     # Caso o usuário escolha continuar, limpa a tela e reinicia o loop.
-    elif resp == "S":
+    elif resp == "1":
         os.system("cls")
